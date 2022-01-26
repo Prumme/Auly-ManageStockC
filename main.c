8cd894c22d4a835c60fde4115a34db0670254fe5
lslimani <lslimani@myges.fr>
2022-01-26 01:08:39 +0100

    Un bon main comme il le faut

--- main.c
+++ main.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
-#include <gtk-3.0/gtk/gtk.h>
 #include <gtk/gtk.h>
 #include <curl/curl.h>
 #include "window.h"
 
-    int
-    main(int argc, char *argv [])
-    {
-        GtkWidget *fenetre_principale = NULL;
-        GtkBuilder *builder = NULL;
-        GError *error = NULL;
-        gchar *filename = NULL;
-        /* Initialisation de la librairie Gtk. */
-        gtk_init(&argc, &argv);
+int
+main(int argc, char *argv[]) {
+    GtkWidget *fenetre_principale = NULL;
+    GtkBuilder *builder = NULL;
+    GError *error = NULL;
+    gchar *filename = NULL;
+    /* Initialisation de la librairie Gtk. */
+    gtk_init(&argc, &argv);
 
-        /* Ouverture du fichier Glade de la fenêtre principale */
-        builder = gtk_builder_new();
+    /* Ouverture du fichier Glade de la fenêtre principale */
+    builder = gtk_builder_new();
 
-        /* Création du chemin complet pour accéder au fichier test.glade. */
-        /* g_build_filename(); construit le chemin complet en fonction du système */
-        /* d'exploitation. ( / pour Linux et \ pour Windows) */
-        filename =  g_build_filename ("/home/aurelien/Bureau/project C/Auly-ManageStockC-main/window_main.glade", NULL);
+    /* Création du chemin complet pour accéder au fichier test.glade. */
+    /* g_build_filename(); construit le chemin complet en fonction du système */
+    /* d'exploitation. ( / pour Linux et \ pour Windows) */
+    filename = g_build_filename("/home/aurelien/Bureau/project C/Auly-ManageStockC-main/window_main.glade", NULL);
 
-        /* Chargement du fichier window_main.glade */
-        gtk_builder_add_from_file (builder, filename, &error);
-        g_free (filename);
-        if (error)
-        {
-            gint code = error->code;
-            g_printerr("%s\n", error->message);
-            g_error_free (error);
-            return code;
-        }
-
-        /* Récupération du pointeur de la fenêtre principale */
-        fenetre_principale = GTK_WIDGET(gtk_builder_get_object (builder, "MainWindow"));
-
-        /* Affectation du signal "destroy" à la fonction gtk_main_quit(); pour la */
-        /* fermeture de la fenêtre. */
-        g_signal_connect (G_OBJECT (fenetre_principale), "destroy", (GCallback)gtk_main_quit, NULL);
-
-        /* Affichage de la fenêtre principale. */
-        gtk_widget_show_all (fenetre_principale);
-
-        gtk_main();
-
-        return 0;
+    /* Chargement du fichier window_main.glade */
+    gtk_builder_add_from_file(builder, filename, &error);
+    g_free(filename);
+    if (error) {
+        gint code = error->code;
+        g_printerr("%s\n", error->message);
+        g_error_free(error);
+        return code;
     }
+
+    /* Récupération du pointeur de la fenêtre principale */
+    fenetre_principale = GTK_WIDGET(gtk_builder_get_object(builder, "MainWindow"));
+
+    /* Affectation du signal "destroy" à la fonction gtk_main_quit(); pour la */
+    /* fermeture de la fenêtre. */
+    g_signal_connect (G_OBJECT(fenetre_principale), "destroy", (GCallback) gtk_main_quit, NULL);
+
+    /* Affichage de la fenêtre principale. */
+    gtk_widget_show_all(fenetre_principale);
+
+    gtk_main();
+
+    return 0;
+}
